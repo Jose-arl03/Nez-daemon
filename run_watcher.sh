ca276cec88af4b0a1d0a87ad49d3e836a5ced216
Jose-arl03 <2130207@upv.edu.mx>
2025-11-03 09:45:53 -0600

    limpieza de archivos del proyecto

--- run_watcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the environment variables the watcher needs
-export WATCH_DIRECTORY="/home/alo03/Estadia/Nez-daemon/services/deployer/app/results"
-export MICTLANX_URI="http://mictlanx-router-0@localhost:60666"
-
-# Run the watcher script using the python from the virtual environment
-/home/alo03/Estadia/Nez-daemon/venv/bin/python3 /home/alo03/Estadia/Nez-daemon/services/watcher/watcher.py
